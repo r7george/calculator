efce3714885db37203acd9986f4f6bd8766f5d3b
Reuben George <reuben_george1998@outlook.com>
2021-11-09 14:53:52 +0000

    script-foundations_mightBeAmmended

---
+++ assets/scripts/main.js
@@ -0,0 +1,38 @@
+const operation = document.querySelector(".calculator__operations-output");
+const output = document.querySelector(".calculator__output");
+
+const ac = document.querySelector(".calculator__ac");
+const plusMinus = document.querySelector(".calculator__sign");
+const percentage = document.querySelector(".calculator__percent");
+const divide = document.querySelector(".calculator__divide");
+const times = document.querySelector(".calculator__multiply");
+const minus = document.querySelector(".calculator__minus");
+const plus = document.querySelector(".calculator__plus");
+const decimal = document.querySelector(".calculator__decimal");
+const equals = document.querySelector(".calculator__equal");
+
+const zero = document.querySelector(".calculator__0");
+const one = document.querySelector(".calculator__1");
+const two = document.querySelector(".calculator__2");
+const three = document.querySelector(".calculator__3");
+const four = document.querySelector(".calculator__4");
+const five = document.querySelector(".calculator__5");
+const six = document.querySelector(".calculator__6");
+const seven = document.querySelector(".calculator__7");
+const eight = document.querySelector(".calculator__8");
+const nine = document.querySelector(".calculator__9");
+
+
+const clear = () => {
+  output.value = undefined;
+}
+
+const getNumber = () => {
+  
+}
+
+
+
+
+
+
