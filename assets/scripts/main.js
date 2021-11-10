632eeafb9d99fd460633bf5900e33120743acbbe
Reuben George <reuben_george1998@outlook.com>
2021-11-10 10:53:06 +0000

    main-operations-working

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,4 +1,4 @@
-const operations = document.querySelector("#operations");
+const display = document.querySelector("#operations");
 const output = document.querySelector("#output");
 
 const buttons = document.querySelectorAll("button");
@@ -7,10 +7,10 @@ const mainOperatorBtns = document.querySelectorAll(".calculator__keypad__main-op
 const equals = document.querySelector(".calculator__keypad__equals");
 const numberBtns = document.querySelectorAll(".calculator__keypad__number");
 
-let currentScreen = "";
-let firstNumber = 21;
-let operator;
-let secondNumber = 7;
+
+let firstNumber = "";
+let operator = "";
+let secondNumber = "";
 const calculator = {
   number1: firstNumber,
   operation: operator,
@@ -20,58 +20,82 @@ const calculator = {
 
 // used to write the equation on the screen
 const writeOperation = (event) => {
-  operations.innerHTML += event.target.value;
-  currentNumber = operations.innerHTML;
+  display.innerHTML += event.target.value;
 }
 
 // used to clear calculator inputs
 const clearCalculation = () => {
-  operations.innerHTML = "";
-  currentNumber = ""
+  display.innerHTML = "";
+  firstNumber = "";
+  secondNumber = "";
   operator="";
+  // output.innerHTML = "";
+}
+
+// sets the firstNumber, operator, and secondNumber variable
+const setVariable = (event) => {
+  if (event.target.className === "calculator__keypad__number" && operator === "") {
+    display.innerHTML += event.target.value;
+    firstNumber += event.target.value;
+    console.log(`${firstNumber}: firstNumber`);
+  }
+  else if (event.target.className === "calculator__keypad__main-operator" && firstNumber != "") {
+    display.innerHTML += event.target.value;
+    operator = event.target.value;
+    console.log(`${operator}: operator`);
+  }
+  else if (event.target.className === "calculator__keypad__number" && firstNumber != "" && operator != "") {
+    display.innerHTML += event.target.value;
+    secondNumber += event.target.value;
+    console.log(`${secondNumber}: secondNumber`);
+  }
 }
 
 // sets the operator variable
-const getOperator = (event) => {
-  if (event.target.value === "/") {
-    operator = "divide";
-    console.log(operator);
-  }
-  else if (event.target.value === "*") {
-    operator = "times";
-    console.log(operator);
-  }
-  else if (event.target.value === "+") {
-    operator = "plus";
-    console.log(operator);
-  }
-  else if (event.target.value === "-") {
-    operator = "minus";
-    console.log(operator);
-  }
-}
+// const getOperator = (event) => {
+//   if (event.target.value === "/") {
+//     operator = "divide";
+//     console.log(operator);
+//   }
+//   else if (event.target.value === "*") {
+//     operator = "times";
+//     console.log(operator);
+//   }
+//   else if (event.target.value === "+") {
+//     operator = "plus";
+//     console.log(operator);
+//   }
+//   else if (event.target.value === "-") {
+//     operator = "minus";
+//     console.log(operator);
+//   }
+// }
 
 // used for the final equal sign
-const finalEqual = (event) => {
-  if (operator === "divide") {
-    output.innerHTML = (firstNumber / secondNumber);
-    firstNumber = (firstNumber / secondNumber);
-    operator = "";    
+const finalEqual = () => {
+  if (operator === "/") {
+    output.innerHTML = (parseFloat(firstNumber) / parseFloat(secondNumber));
+    firstNumber = toString(parseFloat(firstNumber) / parseFloat(secondNumber));
+    operator = ""
+    display.innerHTML = "";    
   }
-  else if (operator === "times") {
-    output.innerHTML = (firstNumber * secondNumber);
-    firstNumber = (firstNumber * secondNumber);
-    operator = "";
+  else if (operator === "*") {
+    output.innerHTML = (parseFloat(firstNumber) * parseFloat(secondNumber));
+    firstNumber = toString(parseFloat(firstNumber) * parseFloat(secondNumber));
+    operator = ""
+    display.innerHTML = "";
   }
-  else if (operator === "plus") {
-    output.innerHTML = (firstNumber + secondNumber);
+  else if (operator === "+") {
+    output.innerHTML = (parseFloat(firstNumber) + parseFloat(secondNumber));
     firstNumber = (firstNumber + secondNumber);
     operator = "";
+
   }
-  else if (operator === "minus") {
-    output.innerHTML = (firstNumber - secondNumber);
-    firstNumber = (firstNumber - secondNumber);
-    operator = "";
+  else if (operator === "-") {
+    output.innerHTML = (parseFloat(firstNumber) - parseFloat(secondNumber));
+    // firstNumber = (firstNumber - secondNumber);
+    // operator = "";
+    // display.innerHTML = "";
   }
 }
 
@@ -79,24 +103,20 @@ const finalEqual = (event) => {
 const calculating = (event) => {
 
   if (event.target.className === "calculator__keypad__number") {
-    // gets the numbers to appear on the screen
     numberBtns.forEach((number) => {
-      number.addEventListener("click", writeOperation);
+      number.addEventListener("click", setVariable);
     });
   }
 
   else if (event.target.className === "calculator__keypad__sub-operator") {
-    // gets the suboperators to appear on the screen
     subOperatorBtns.forEach((operator) => {
       operator.addEventListener("click", writeOperation);
     });
   }
 
   else if (event.target.className === "calculator__keypad__main-operator") {
-    // gets the main operators to appear on the screen
     mainOperatorBtns.forEach((operator) => {
-      operator.addEventListener("click", writeOperation);
-      operator.addEventListener("click", getOperator);
+      operator.addEventListener("click", setVariable);
     });
   }
 
