51bf8d3e7f663e295380b3a91a436114cbd53da8
Reuben George <reuben_george1998@outlook.com>
2021-11-25 18:24:34 +0000

    BEM-and-script-changes-streamlining

--- assets/scripts/main.js
+++ assets/scripts/main.js
@@ -1,118 +1,89 @@
 const display = document.querySelector("#operations");
 const output = document.querySelector("#output");
 
-const buttons = document.querySelectorAll("button");
-const subOperatorBtns = document.querySelectorAll(".calculator__keypad__sub-operator");
-const mainOperatorBtns = document.querySelectorAll(".calculator__keypad__main-operator");
-const equals = document.querySelector(".calculator__keypad__equals");
-const numberBtns = document.querySelectorAll(".calculator__keypad__number");
+const subOperatorBtns = document.querySelectorAll(".calculator__sub-operator");
+const mainOperatorBtns = document.querySelectorAll(".calculator__main-operator");
+const equals = document.querySelector(".calculator__equals");
+const numBtns = document.querySelectorAll(".calculator__number");
 
 let firstNumber = "";
 let operator = "";
 let secondNumber = "";
 
 
-// sets the firstNumber, operator, and secondNumber variable
-const setVariable = (event) => {
-  if (event.target.className === "calculator__keypad__number" && operator === "") {
-    console.log(event.target.value == "." && !firstNumber.includes("."))
-    if(event.target.value == "."){
-      if (!firstNumber.includes(".")) {
-        display.innerHTML += event.target.value;
-        firstNumber += event.target.value;
-      }
-    }
-    else {
-      display.innerHTML += event.target.value;
-      firstNumber += event.target.value;
-    }
+// gets the value for any event
+const getValue = (event) => {
+  return event.target.value;
+}
+
+// gets the id for the event in question
+const getId = (event) => {
+  return event.target.id;
+}
+
+// resets a variable to an empty string
+const resetVariable = (variable) => {
+  variable = "";
+  return variable;
+}
+
+
+// function for the numberBtn event listener
+const handleNumClick = (event) => {
+  const val = getValue(event);
+  // to stop decimals appearing multiple times
+  if (val == "." && firstNumber.includes(val) && secondNumber == "") {
+    return;
+  } else if (val == "." && secondNumber.includes(val)) {
+    return;
   }
-  else if (event.target.className === "calculator__keypad__main-operator" && firstNumber != "" && operator === "") {
-    display.innerHTML += event.target.value;
-    operator = event.target.value;
-  }
-  else if (event.target.className === "calculator__keypad__main-operator" && firstNumber != "" && operator != "") {
-    display.innerHTML = `${firstNumber}${event.target.value}`;
-    operator = event.target.value;
-    secondNumber = "";
-  }
-  else if (event.target.className === "calculator__keypad__number" && firstNumber != "" && operator != "") {
-    if(event.target.value == "."){
-      if (!secondNumber.includes(".")) {
-        display.innerHTML += event.target.value;
-        secondNumber += event.target.value;
-      }
-    }
-    else {
-      display.innerHTML += event.target.value;
-      secondNumber += event.target.value;
-    }
-    
+
+  if (operator == "") {
+    firstNumber += val;
+    display.innerHTML = firstNumber;
+  } else if (firstNumber != "" && operator != "") {
+    secondNumber += val;
+    display.innerHTML = `${firstNumber}${operator}${secondNumber}`;
   }
 }
 
 
-// used for the final equal sign
-const clickingEqual = () => {
+// function for the mainOperatorBtns event listener
+const handleMainClick = (event) => {
+  const val = getValue(event);
   if (secondNumber != "") {
-    if (operator === "/") {
-      const num = (parseFloat(firstNumber) / parseFloat(secondNumber)).toPrecision(6);
-      output.innerHTML = num;
-      firstNumber = num.toString();
-      operator = "";
-      secondNumber = "";
-    }
-    else if (operator === "*") {
-      const num = (parseFloat(firstNumber) * parseFloat(secondNumber)).toPrecision(6);
-      output.innerHTML = num;
-      firstNumber = num.toString();
-      operator = "";
-      secondNumber = "";
-    }
-    else if (operator === "+") {
-      const num = (parseFloat(firstNumber) + parseFloat(secondNumber)).toPrecision(6);
-      output.innerHTML = num;
-      firstNumber = num.toString();
-      operator = "";
-      secondNumber = "";
-    }
-    else if (operator === "-") {
-      const num = (parseFloat(firstNumber) - parseFloat(secondNumber)).toPrecision(6);
-      output.innerHTML = num;
-      firstNumber = num.toString();
-      operator = "";
-      secondNumber = "";
-    }
+    return;
+  }
+  if (firstNumber != "") {
+    operator = val;
+    display.innerHTML = `${firstNumber}${operator}`;
   }
 }
 
 
-// used for giving each suboperator the relevant action
-const subOperators = (event) => {
-  // for clearing input (AC button)
-  if (event.target.id === "ac") {
+// function for the subOperatorBtns event listener
+const handleSubClick = (event) => {
+  // for AC
+  if (getId(event) == "ac") {
     display.innerHTML = "";
-    firstNumber = "";
-    secondNumber = "";
-    operator="";
+    secondNumber = resetVariable(secondNumber);
+    operator = resetVariable(operator);
   }
-
-  // for the plusMinus operator
-  else if (event.target.id === "plusMinus") {
-    if (firstNumber != "" && operator === "") {
+  // for +/-
+  else if (getId(event) == "plusMinus") {
+    if (firstNumber != "" && operator == "") {
       const num = (-parseFloat(firstNumber)).toString();
       display.innerHTML = `${num}`;
       firstNumber = num;
-    }
+    } 
     else if (secondNumber != "" && operator != "") {
       const num = (-parseFloat(secondNumber)).toString();
       display.innerHTML = `${firstNumber}${operator}${num}`;
       secondNumber = num;
     }
   }
-
-  // for the percentage operator
-  else if (event.target.id === "percent") {
+  // for %
+  else if (getId(event) == "percent") {
     if (firstNumber != "" && operator === "") {
       const num = (parseFloat(firstNumber)/100).toString();
       display.innerHTML = `${num}`;
@@ -124,46 +95,56 @@ const subOperators = (event) => {
       secondNumber = num;
     }
   }
-
 }
 
 
-// used to set off the flow based on what type of button is pressed
-const buttonClassDetermining = (event) => {
-
-  if (event.target.className === "calculator__keypad__number") {
-    numberBtns.forEach((number) => {
-      number.addEventListener("click", setVariable);
-    });
-  }
-
-  else if (event.target.className === "calculator__keypad__sub-operator") {
-    subOperatorBtns.forEach((operator) => {
-      operator.addEventListener("click", subOperators);
-    });
-  }
-
-  else if (event.target.className === "calculator__keypad__main-operator") {
-    mainOperatorBtns.forEach((operator) => {
-      operator.addEventListener("click", setVariable);
-    });
-  }
-
-  else if (event.target.className === "calculator__keypad__equals") {
-    equals.addEventListener("click", clickingEqual);
-  }
-  
-}
-
-
-buttons.forEach((btn) => {
-  btn.addEventListener("click", buttonClassDetermining);
-});
-
-subOperatorBtns[0].addEventListener("dblclick", () => {
+// function for the AC button DblClick event
+const handleAcDblClick = () => {
   output.innerHTML = "";
   display.innerHTML = "";
-  firstNumber = "";
-  secondNumber = "";
-  operator="";
+  firstNumber = resetVariable(firstNumber);
+  secondNumber = resetVariable(secondNumber);
+  operator = resetVariable(operator);
+}
+
+
+// function for the equal event listener
+const handleEqualClick = () => {
+  if (secondNumber == "") {
+    return;
+  }
+
+  let finalNumber = 0;
+  if (operator === "/") {
+    finalNumber = (parseFloat(firstNumber) / parseFloat(secondNumber)).toPrecision(6);
+  } else if (operator === "*") {
+    finalNumber = (parseFloat(firstNumber) * parseFloat(secondNumber)).toPrecision(6);
+  } else if (operator === "+") {
+    finalNumber = (parseFloat(firstNumber) + parseFloat(secondNumber)).toPrecision(6);
+  } else if (operator === "-") {
+    finalNumber = (parseFloat(firstNumber) - parseFloat(secondNumber)).toPrecision(6);
+  }
+  
+  output.innerHTML = finalNumber;
+  firstNumber = finalNumber.toString();
+  operator = resetVariable(operator);
+  secondNumber = resetVariable(secondNumber);
+}
+
+
+// ALl the required event listeners
+numBtns.forEach((button) => {
+  button.addEventListener("click", handleNumClick);
 });
+
+mainOperatorBtns.forEach((button) => {
+  button.addEventListener("click", handleMainClick);
+});
+
+subOperatorBtns.forEach((button) => {
+  button.addEventListener("click", handleSubClick);
+});
+
+document.querySelector("#ac").addEventListener("dblclick", handleAcDblClick);
+
+equals.addEventListener("click", handleEqualClick);
